7669702cc89578777bb76177fdcb824075c4b808
Shaun Williams <shaun.e.williams@gmail.com>
2015-04-18 08:50:54 -0500

    add unique hashes for each file example

--- add-example-hashes.php
+++ add-example-hashes.php
@@ -1,6 +1,4 @@
 <?php
-// run this file to make sure examples have unique hashes
-// (coded on an airplane with chris because he knows php)
 
 function h() {
   return substr(md5(rand()), 0,6);
@@ -10,10 +8,11 @@ $files = glob("*.cljsdoc");
 
 foreach($files as $filename) {
   echo "parsing $filename\n";
-  $content = file_get_contents($filename);
-  $new = str_replace("Example\n", "Example#".h()."\n", $content);
-  echo $new;
-  file_put_contents($filename, $new);
+  $lines = file($filename);
+  for($i=0; $i<count($lines); $i++) {
+    $lines[$i] = str_replace("Example\n", "Example#".h()."\n", $lines[$i]);
+  }
+  file_put_contents($filename, join("",$lines));
 }
 
 ?>
